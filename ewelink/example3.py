90cb4481dca64649e25b385b5052de3fbc9d2835
tmorse305 <38409250+tmorse305@users.noreply.github.com>
2025-10-05 21:16:19 -0400

    Create example3.py

---
+++ ewelink/example3.py
@@ -0,0 +1,27 @@
+
+import ewelink
+from ewelink import Client, DeviceOffline, Power
+
+
+@ewelink.login('Tea4twoA@eWL','tmorse305@comcast.net')
+async def main(client: Client):
+    print(client.region)
+    print(client.user.info)
+    print(client.devices)
+
+    device =  client.get_device('100118ee7e') #Mickey's ID
+    print(device.params)
+        # Raw device specific properties
+        # can be accessed easily like: device.params.switch or device.params['startup'] (a subclass of dict)
+
+    print(device.state)
+    print(device.created_at)
+    print("Brand Name:", device.brand.name, "Logo URL:", device.brand.logo.url)
+    print("Device online?", device.online)
+
+    try:
+        #await device.on()
+        await device.edit(Power.on[0])
+        print("Power on sent")
+    except DeviceOffline:
+        print("Device is offline!")
